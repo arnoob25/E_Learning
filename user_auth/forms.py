9ebd32c2276524d3be56fad8a38e3a23befea326
Arnob <arnob@gmail.com>
2024-01-07 08:13:59 +0600

    Created the user)auth functionalities

---
+++ user_auth/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+class SignupForm(UserCreationForm):
+
+    group = forms.ModelChoiceField(
+        queryset = Group.objects.filter(
+            name__in = ['teacher', 'student',]
+        ), label = 'Signup as:', required = True, blank = False, widget=forms.RadioSelect
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['group', 'username', 'password1', 'password2',]
+        labels = {
+            'group': 'Sign up as:',
+        }
+
+    def save(self, commit = True):
+        user = super().save(commit = False)
+        if commit:
+            user.save()
+            user.groups.add(self.cleaned_data['group'])
+        return user
---
+++ user_auth/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import Group
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from . import forms
+from django.contrib.auth import get_user_model
+
+# Create your views here.
+
+class SignupView(CreateView):
+    form_class = forms.SignupForm
+    template_name = 'user_auth/user_auth.html'
+    success_url = reverse_lazy('user_auth:login')
+
+class LoginView(LoginView):
+    template_name = 'user_auth/user_auth.html'
