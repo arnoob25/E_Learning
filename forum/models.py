ec62d820f9f546088c3b7e82e7c697c11299f063
Arnob <arnob@gmail.com>
2024-01-17 10:26:09 +0600

    Crated the Question model for the forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length = 200)
+    body = models.TextField()
+    created_by = models.ForeignKey(get_user_model(), blank = True, on_delete = models.CASCADE)
+    created_at = models.DateField(auto_now_add = True)
+    slug = models.SlugField(max_length = 200, blank = True, unique = True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
