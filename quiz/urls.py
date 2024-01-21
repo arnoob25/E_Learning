42e54f5f8d399d8851d9fe15bc8f78ee9291db19
Arnob <arnob@gmail.com>
2024-01-21 10:49:07 +0600

    Now teachers can review students' attempts at quizzes.

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,16 +5,19 @@ from django.views.generic.base import RedirectView
 app_name = 'quiz'
 
 urlpatterns = [
+    # all users
+    path('list/', views.ListQuizzes.as_view(), name = 'list'),
+
     # teacher - arranging the quiz
     path('new/', views.NewQuiz.as_view(), name = 'new_quiz'),
     path('new/<slug:slug>', views.newQuestions, name = 'new_questions'),
+
+    # teacher - evaluating the quiz
+    path('review/<slug:slug>', views.reviewStudentAttempts, name = 'review'),
     
     # student - attempting the quiz
     path('attempt/<slug:slug>', views.attemptQuiz, name = 'attempt'),
     path('summary/<slug:slug>', views.displaySummary, name = 'summary'),
-
-    # all users
-    path('list/', views.ListQuizzes.as_view(), name = 'list'),
 
     # redirecting the users to quiz
     path('', RedirectView.as_view(pattern_name = 'quiz:list')),
